cf8db34e9a00a6d6ed54953ae271e76523d16e69
sunhyeok26 <toddlfgovl@gmail.com>
2022-11-28 08:59:46 +0000

    mysql

---
+++ node/mysql-sync/mysql.js
@@ -0,0 +1,14 @@
+var mysql = require("sync-mysql");
+const env = require("dotenv").config({ path:"../../.env"});
+
+var connection = new mysql({
+	host : process.env.host,
+	port : process.env.port,
+	user : process.env.user,
+	password : process.env.password,
+	database : process.env.database
+})
+
+// Select all rows from st_info table
+let result = connection.query("SELECT * FROM st_info");
+console.log(result);
---
+++ node/mysql2/mysql.js
@@ -0,0 +1,61 @@
+const mysql = require("mysql2");
+const env = require("dotenv").config({ path:"../../.env" });
+
+const db = async () => {
+  try {
+   // db connection
+   let connection = await mysql.createConnection({
+         host : process.env.host,
+                  user : process.env.user,
+                  password : process.env.password,
+                  database : process.env.database
+      });
+
+      // Select all rows from st_info table
+      let [rows, fields] = await connection.query("SELECT * FROM st_info");
+      console.log(rows);
+
+      // insert data
+      let data = {
+        ST_ID : "20204",
+        NAME : "Moon",
+        DEPT : "Computer"
+      };
+
+      // insert data into st_info table
+      let [results] = await connection.query(
+        "INSERT INTO st_info SET ?",
+        data
+      );
+      // inserted data's id
+      let insertId = data.ST_ID;
+
+      // Select all rows from st_info table
+      [rows, fields] = await connection.query("SELECT * FROM st_info");
+      console.log(rows);
+
+      // update row
+      [results] = await connection.query("UPDATE st_info SET DEPT=? WHERE ST_ID=?", [
+        "Game",
+        insertId,
+      ]);
+
+      // Select all rows from st_info table
+      [rows, fields] = await connection.query("SELECT * FROM st_info");
+      console.log(rows);
+
+      // delete row
+      [results] = await connection.query("DELETE FROM st_info WHERE ST_ID=?", [
+          insertId,
+      ]);
+
+      // Select all rows from st_info table
+      [rows, fields] = await connection.query("SELECT * FROM st_info");
+      console.log(rows);
+
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  db();
